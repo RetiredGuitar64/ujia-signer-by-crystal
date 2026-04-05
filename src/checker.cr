ff08a4957ec012176c418ca7a3feea5e0698f5a1
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-05 20:08:16 +0800

    checker 目前完成差不多了

--- src/checker.cr
+++ src/checker.cr
@@ -1,6 +1,11 @@
 require "./accounts.cr"
 require "http/client"
 require "json"
+require "log"
+
+SLEEP_GAP_IN_LOOP = 2
+# 签到id正则
+COURSE_SIGN_IN_ID_RE = /"courseSignInId"\s*:\s*"([0-9a-f]{32})"/
 
 class Checker
   getter name, token
@@ -21,9 +26,28 @@ class Checker
       "connection"     => "Keep-Alive",
     }
 
-    response = HTTP::Client.get(url, headers)
-    puts response.status_code
-    json = JSON.parse(response.body)
-    pp json["data"]
+    Log.info{"轮询已开始..."}
+    loop do
+      response = HTTP::Client.get(url, headers)
+
+      if response.status_code == 200
+        if courseSignInId = detect_courseSignInId(response)
+          Log.info{"探测到签到: #{courseSignInId}"}
+        end
+      else
+        Log.error{"状态码: #{response.status_code}, \"#{@name}\" 的token \"#{@token}\" 可能已失效"}
+      end
+
+      sleep SLEEP_GAP_IN_LOOP
+    end
+  end
+
+  # 探测是否有签到，这个方法会很频繁运行, 需要性能
+  def detect_courseSignInId(response : HTTP::Client::Response) : String?  # 提前指定类型
+    if match = COURSE_SIGN_IN_ID_RE.match(response.body)
+      # 匹配到并返回签到id
+      return match[1]
+    end
+    nil
   end
 end
--- src/main.cr
+++ src/main.cr
@@ -1,6 +1,4 @@
 require "./checker.cr"
 
 checker = Checker.new
-p checker.name
-p checker.token
 checker.run
