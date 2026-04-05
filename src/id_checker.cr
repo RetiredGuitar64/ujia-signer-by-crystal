e793d5286904c385122863a20ee45365528e6a73
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-05 20:56:27 +0800

    获取签到码-完工

--- src/id_checker.cr
+++ src/id_checker.cr
@@ -6,6 +6,7 @@ require "log"
 SLEEP_GAP_IN_LOOP = 2
 # 签到id正则
 COURSE_SIGN_IN_ID_RE = /"courseSignInId"\s*:\s*"([0-9a-f]{32})"/
+CODE_DISTANCE_RE = /"codeDistance"\s*:\s*"(\d{3,4})"/
 
 class Checker
   getter name, token
@@ -14,10 +15,10 @@ class Checker
   end
 
   def run
-    url = "https://www.eduplus.net/api/course/courses/v1/study?types=Theory,Train"
     cookie = "SESSION=#{@token}"
 
-    headers = HTTP::Headers{
+    id_check_url = "https://www.eduplus.net/api/course/courses/v1/study?types=Theory,Train"
+    check_headers = HTTP::Headers{
       "accept"         => "application/json, text/plain, */*",
       "x-access-token" => @token,
       "cookie"         => cookie,
@@ -28,11 +29,19 @@ class Checker
 
     Log.info{"轮询已开始..."}
     loop do
-      response = HTTP::Client.get(url, headers)
+      response = HTTP::Client.get(id_check_url, check_headers)
 
       if response.status_code == 200
         if courseSignInId = detect_courseSignInId(response)
           Log.info{"探测到签到: #{courseSignInId}"}
+          # 开始获取签到码
+          code_check_url = "https://www.eduplus.net/api/course/clock_in/#{courseSignInId}/student" # codecheck的url
+          response_of_code_check = HTTP::Client.get(code_check_url, check_headers)
+          codeDistance = detect_codeDistance(response_of_code_check)
+
+          # 进入签到流程
+          puts codeDistance
+
         end
       else
         Log.error{"状态码: #{response.status_code}, \"#{@name}\" 的token \"#{@token}\" 可能已失效"}
@@ -49,5 +58,14 @@ class Checker
       return match[1]
     end
     nil
+  end
+
+  def detect_codeDistance(response : HTTP::Client::Response) : String
+    if match = CODE_DISTANCE_RE.match(response.body)
+      return match[1]
+    else
+      Log.warn{"未匹配到签到码字段，未知错误，将默认以普通签到进行"}
+      return "200"
+    end
   end
 end
