11680683481e167b6bfc9201e45240b54e48fa4e
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-07 17:18:01 +0800

    加注释

--- src/status.cr
+++ src/status.cr
@@ -2,19 +2,22 @@ require "mutex"
 
 class Status
 
+  # 发给web的消息
   @message : String
 
   def initialize
     @status = Mutex.new
-    @message = "Error: \nInit message\n签到状态发送出错\n尽快修复！！！"
+    @message = "Error: \nInit message\n签到状态发送出错\n尽快修复！！！" # 初始化的消息
   end
 
+  # web显示靠这个方法
   def web_show
     @status.synchronize do
       @message
     end
   end
 
+  # 直接显示
   def display(message : String)
     @status.synchronize do
       @message = message
--- src/web.cr
+++ src/web.cr
@@ -1,16 +1,23 @@
 require "http/server"
 require "log"
 
+# 启动IP,默认本机
 BIND_ADDRESS = "0.0.0.0"
+# 启动端口
 BIND_PORT = 18888
 
 class Web
+  # web 会从@status里的@message去读取信息并显示
   def initialize(@status : Status)
   end
 
+  # 启动方法
   def start
+    # 开一个server
     web = HTTP::Server.new do |context|
+      # 默认回应html
       context.response.content_type = "text/html; charset=utf-8"
+      # 渲染html
       context.response.print <<-HTML
       <!doctype html>
       <html>
@@ -26,8 +33,10 @@ class Web
       HTML
     end
 
+    # 绑定的地址
     address = web.bind_tcp(BIND_ADDRESS, BIND_PORT)
 
+    # 开一个新的fiber来启动web, 防止主循环和web互相block住
     spawn do
       web.listen
     end
