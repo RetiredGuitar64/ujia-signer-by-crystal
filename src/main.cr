a55626b5303e5f17eb770bf9ff8d2556339846a3
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-28 16:37:07 +0800

    删掉temp_auth, 改为加-u参数来开始认证并获取token

--- src/main.cr
+++ src/main.cr
@@ -4,12 +4,40 @@ require "./checker.cr"
 require "./web.cr"
 require "./status.cr"
 require "./accounts_reader.cr"
+require "./auth_saver.cr"
 
+# 默认认证模式为关
 auth_mode : Bool = false
 
 OptionParser.parse do |parser|
   parser.banner = "Usage: ujiacrystal [arguments]"
-  parser.on("-t", "-get-token", "Get your account's token"){ auth_mode = true }
+  parser.on("-u", "--get-token", "Get your account's token"){ auth_mode = true }
+  parser.on("-h", "--help", "Show this help"){ puts parser; exit }
+end
+
+# -u 认证并获取token
+if auth_mode
+  auth = AuthSaver.new
+  puts "请输入手机号: "
+  phone : String = gets || ""
+
+  if phone.size != 11
+    puts "手机号位数不正确！"
+    exit(1)
+  end
+
+  puts "请输入密码："
+  password : String = gets || ""
+
+  print "\e[2J\e[H"
+  STDOUT.flush
+
+  puts "开始认证..."
+  token = auth.auth_with_password(phone, password)
+  sleep 1.seconds
+  Log.info{ "认证完毕：请自行将token加入账号文件" }
+
+  exit
 end
 
 # 读取账号
