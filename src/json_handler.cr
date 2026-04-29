fe43d07e5d3ea077f1fdf569fb1908cb7096813b
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-29 16:32:15 +0800

    补丁

--- src/json_handler.cr
+++ src/json_handler.cr
@@ -4,6 +4,8 @@ class JsonHandler
   def self.token_avaliable?(body : String) : Bool
     json = JSON.parse(body)
     course_data = json["data"].as_a
+    return false if course_data.empty?
+
     course_data.each do |course|
       return false if course["courseSignInOpen"]?.nil?
     end
